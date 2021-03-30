c19a98838394f785f21b0cb9a13af638892d8a13
Boanerges Junior <boanerges_junior@hotmail.com>
2021-03-30 18:36:40 -0300

    Teste das rotas da API

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,43 +1,68 @@
 const express = require('express')
+const {uuid} = require('uuidv4')
 
 const app = express()
 const port = 3333
 
 app.use(express.json())
 
+const projects = []
+
+
+
 app.get('/projects', (request, response) => {
-   return response.send([
-       'Projeto1',
-       'Projeto2'
-   ]) 
+    const { nome } = request.query
+
+    const results = nome
+    ? projects.filter(project => project.nome === nome)
+    : projects
+    
+    return response.send(results) 
 })
 
 app.post('/projects', (request, response) => {
     const {nome, idade} = request.body
     
-    console.log(nome)
-    console.log(idade)
+    const project = {id: uuid(), nome, idade}
 
-    return response.send([
-        'Projeto1',
-        'Projeto2',
-        'Projeto3'
-    ])
+    projects.push(project)
+
+    return response.send(project)
 })
 
 app.put('/projects/:id', (request, response) => {
-    return response.send([
-        'Projeto4',
-        'Projeto2',
-        'Projeto3'
-    ])
+    const {id} = request.params
+    const {nome, idade} = request.body
+    
+    const projectIndex = projects.findIndex(project => project.id === id)
+
+    if(projectIndex < 0){
+        return response.status(400).send({error: 'Project not found'})
+    }
+
+    const project = {
+        id,
+        nome,
+        idade 
+    }
+
+    projects[projectIndex] = project
+
+    return response.send(project)
 })
 
 app.delete('/projects/:id', (request, response) => {
-    return response.send([
-        'Projeto2',
-        'Projeto3'
-    ])
+    const {id} = request.params
+
+    const projectIndex = projects.findIndex(project => project.id === id)
+    
+    if(projectIndex < 0){
+        return response.status(400).send({error: 'Project not found'})
+    }
+
+    projects.splice(projectIndex, 1)
+    
+    return response.status(204).send();
 })
 
 app.listen(port, () => {
