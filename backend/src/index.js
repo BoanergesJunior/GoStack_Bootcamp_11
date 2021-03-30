5ae8b393d76c1d8dc30a5b846e72c99d5d978309
Boanerges Junior <boanerges_junior@hotmail.com>
2021-03-30 16:51:27 -0300

    Teste de requisições HTTP

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,9 +3,43 @@ const express = require('express')
 const app = express()
 const port = 3333
 
+app.use(express.json())
 
 app.get('/projects', (request, response) => {
-   return response.send({message:'Hello world'}) 
+   return response.send([
+       'Projeto1',
+       'Projeto2'
+   ]) 
 })
 
-app.listen(port)
+app.post('/projects', (request, response) => {
+    const {nome, idade} = request.body
+    
+    console.log(nome)
+    console.log(idade)
+
+    return response.send([
+        'Projeto1',
+        'Projeto2',
+        'Projeto3'
+    ])
+})
+
+app.put('/projects/:id', (request, response) => {
+    return response.send([
+        'Projeto4',
+        'Projeto2',
+        'Projeto3'
+    ])
+})
+
+app.delete('/projects/:id', (request, response) => {
+    return response.send([
+        'Projeto2',
+        'Projeto3'
+    ])
+})
+
+app.listen(port, () => {
+    console.log(`Back-end started on: http://localhost:${port}`)
+})
