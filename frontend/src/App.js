b2a8468e1cdb97759ab6d61e667e5e929ae58f4d
Boanerges Junior <boanerges_junior@hotmail.com>
2021-04-03 22:02:17 -0300

    Parametros em componentes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,11 +3,22 @@ import Header from './Components/Header'
 function App() {
     return (
         <React.Fragment>
-            <Header />
-            <Header />
+            <Header title="Homepage">
+                <ul>
+                    <li>Homepage</li>
+                    <li>Projects</li>
+                </ul>
+            </Header>
+
+            <Header title="Projects">
+                <ul>
+                    <li>Homepage</li>
+                    <li>Login</li>
+                    <li>Projects</li>
+                </ul>
+            </Header>
         </React.Fragment>
     )
-        
 }
 
 export default App
