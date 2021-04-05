b773bc315f72d4294bac1152450e3999d8809b3f
Boanerges Junior <boanerges_junior@hotmail.com>
2021-04-05 17:25:45 -0300

    Rota post

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,38 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Header from './Components/Header'
 import './App.css'
 import backgroundImage from './assets/background.jpg'
+import api from './services/api'
 
 function App() {
 
-    const [projects, setProjects] = useState(['Denvolviment de app', 'Front-end web'])
+    const [projects, setProjects] = useState([])
 
-    function heandleAddProject() {
+    useEffect(() => {
+        api.get('projects').then(response => {
+            setProjects(response.data)
+        })
+    }, [])
+
+    async function heandleAddProject() {
         // projects.push(`Novo projeto ${Date.now()}`)
-        setProjects([...projects, `Novo projeto ${Date.now()}`])
+        // setProjects([...projects, `Novo projeto ${Date.now()}`])
 
-        console.log(projects);
+        const response = await api.post('projects', {
+            nome: `Novo projeto ${Date.now()}`,
+            tipo: "Projeto mobile"
+        })
+
+        const project = response.data
+        setProjects([...projects,  project])
     }
 
     return (
         <React.Fragment>
             <Header title="Projects"/>
 
-            <img width="300" src={backgroundImage} alt="Imagem de fundo"/>
-
             <ul>
-                {projects.map(project => <li key={project}>{project}</li>)}
+                {projects.map(project => <li key={project.id}>{project.nome}</li>)}
             </ul>
 
             <button type="button" onClick={heandleAddProject}>Adicionar projeto</button>
